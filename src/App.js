5f2c68880aa2a83ae86569485e0fd2e29a49f100
git4res <akuzin20@gmail.com>
2022-12-16 17:57:52 +0300

    commit1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-это мастер здесь тоже много кода
-      мастер мастер мастер
-      это дэ я тут моного кдатвотыывтсдлы
+
       <div>Hello</div>
     </div>
   );
